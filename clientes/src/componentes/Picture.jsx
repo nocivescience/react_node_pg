bfdd3d881a73fa86897d5fb78eab87b13c2c10c6
noceive <nocivescience@gmail.com>
2023-01-09 03:57:31 -0300

    hasta todo bien voy a agregar el mouse pa rastreo

---
+++ clientes/src/componentes/Picture.jsx
@@ -0,0 +1,8 @@
+import '../estaticos/css/Picture.css'
+export const Picture=(props)=>{
+    return (
+        <div className='image-container'>
+            <img src={props.imagen}></img>
+        </div>
+    )
+}
---
+++ clientes/src/componentes/TaskForm.jsx
@@ -0,0 +1,32 @@
+import "../estaticos/css/TaskForm.css";
+import { Picture } from "../componentes/Picture";
+import Asiatica from '../estaticos/images/fondo3.jpg'
+export const TaskForm = () => {
+  return (
+    <div className="pantalla">
+      <div className="izquierda">
+        <h1>Formulario</h1>
+        <form>
+          <div className="form-grupo">
+            <label>Title</label>
+            <input></input>
+          </div>
+          <div className="form-grupo">
+            <label>Title</label>
+            <textarea
+              rows='5'
+            ></textarea>
+          </div>
+          <div className="form-grupo form-boton">
+            <button>Click Here</button>
+          </div>
+        </form>
+      </div>
+      <div className="derecha">
+        <div>
+          <Picture imagen={Asiatica}></Picture>
+        </div>
+      </div>
+    </div>
+  );
+};
